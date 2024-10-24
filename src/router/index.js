5a47a5708b032843618cf27c90fcc2ddea73b7f9
Mohamed Agnide <fadulagnide01@gmail.com>
2024-10-24 16:16:24 +0200

    creation de la page de connexion

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,37 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
+let isAuthenticated = false;
+const routes = [
+  {
+    path: '/home',
+    component: HomeView,
+    meta: { requiresAuth: true }, 
+  },
+  {
+    path: '/',
+    component: LoginView
+  },
+
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/LoginView.vue'),
-    },
-  ],
-})
+  routes,
+});
 
-export default router
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth) && !isAuthenticated) {
+    next('/'); 
+  } else {
+    next(); 
+  }
+});
+
+
+export function setAuthentication(status) {
+  isAuthenticated = status;
+}
+
+export default router;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,5 +3,8 @@
 </script>
 
 <template>
-  
+<div>
+<h1>je suis le composant home</h1>
+</div>
+
 </template>
